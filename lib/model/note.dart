92aa06d76340aa613660854bbc46b668f2eb01c4
Hung <a921311973@gmail.com>
2025-05-27 16:26:04 +0800

    note

---
+++ lib/model/note.dart
@@ -0,0 +1,11 @@
+class Note {
+  final String title;
+  final String description;
+  final DateTime dateTime;
+
+  Note({
+    required this.title,
+    required this.dateTime,
+    required this.description,
+  });
+}
